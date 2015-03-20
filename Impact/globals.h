510674d35c255414e99821e4b5b6957654bd72a1
Oliver Lau <ola@ct.de>
2015-03-20 13:42:34 +0100

    v1.0-BETA21

--- Impact/globals.h
+++ Impact/globals.h
@@ -27,7 +27,7 @@
 
 namespace Impact {
 
-#define IMPACT_VERSION "1.0-BETA20"
+#define IMPACT_VERSION "1.0-BETA21"
 
 #define ResourcesDir std::string("resources")
 #define SoundFXDir ResourcesDir + "/soundfx"
