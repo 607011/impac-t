80221fafe838fe99ff9d66c547347d7676ad820a
Oliver Lau <ola@ct.de>
2015-03-23 15:03:51 +0100

    DLLs will be packaged from Release\* New post build command "prepare.bat" to copy level and sound fx files to %APPDATA%.

---
+++ prepare.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET CWD=%~dp0
+SET MYPATH=%CWD:~0,-1%
+
+ECHO Creating directories ...
+IF NOT EXIST "%APPDATA%\Impact\" ( MD %APPDATA%\Impact )
+IF NOT EXIST "%APPDATA%\Impact\levels\" ( MD "%APPDATA%\Impact\levels" )
+IF NOT EXIST "%APPDATA%\Impact\soundfx\" ( MD "%APPDATA%\Impact\soundfx" )
+
+ECHO Copying levels ...
+@COPY "%MYPATH%\Impact\resources\levels\*.zip" "%APPDATA%\Impact\levels"
+
+echo Copying sound fx ...
+@COPY "%MYPATH%\Impact\resources\soundfx\*.ogg" "%APPDATA%\Impact\soundfx"
