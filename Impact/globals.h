8d37cce091b878fbc4139260ddf77d0122f3d1b0
Oliver Lau <ola@ct.de>
2015-04-01 12:21:30 +0200

    v1.0-BETA28

--- Impact/globals.h
+++ Impact/globals.h
@@ -27,7 +27,7 @@
 
 namespace Impact {
 
-#define IMPACT_VERSION "1.0-BETA27"
+#define IMPACT_VERSION "1.0-BETA28"
 
 #define ResourcesDir std::string("resources")
 #define ImagesDir ResourcesDir + "/images"
